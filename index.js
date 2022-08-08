a6ba379dc2f8b09d7ec80c0defd6f828dd160af5
Schuler Small <schuler.small.10@gmail.com>
2022-08-08 11:30:15 -0500

    installed axios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { restart } = require('nodemon');
+const axios = require('axios');
 const querystring = require('query-string');
 const app = express();
 const port = 8888;
