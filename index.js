527242da0bbcb486966b4f9b9c60aec5a151f670
Schuler Small <schuler.small.10@gmail.com>
2022-08-08 11:36:50 -0500

    added axios request

--- index.js
+++ index.js
@@ -51,7 +51,34 @@ app.get('/login', (req, res) => {
 
 // CALLBACK
 app.get('/callback', (req, res) => {
-  res.send('Callback')
+  const code = req.query.code || null;
+
+  // https://github.com/axios/axios#request-config
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI,
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}: ${CLIENT_SECRET}`
+      ).toString('base64')}`
+    }
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch((error) => {
+      res.send(error)
+    });
 });
 
 app.listen(port, () => {
