def4deda2bc803d249d523f5df7fb0fcb42adc4b
Schuler Small <schuler.small.10@gmail.com>
2022-08-09 16:55:06 -0500

    added refresh fetch to useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,12 @@ function App() {
     console.log(accessToken);
     console.log(refreshToken);
 
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
+    }
   }, []);
 
   return (
