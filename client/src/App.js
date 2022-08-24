a821214dc3f5e0c21d337de8a20502eca3ac7fa4
Schuler Small <schuler.small.10@gmail.com>
2022-08-24 16:08:46 -0500

    add profile ui

--- client/src/App.js
+++ client/src/App.js
@@ -27,14 +27,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         {!token ? (
-        
-        <a className="App-link" href="http://localhost:8888/login">
-          Login to Spotify
-        </a>
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
         ) : (
           <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}> Log Out</button>
+            <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Profile Pic"/>
+                )}
+              </div>
+            )}
           </>
         )}
       </header>
