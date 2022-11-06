306de9326e6c5747a72bcccaca6a4ad9b38e1734
Schuler Small <ssmall@goodleap.com>
2022-11-05 21:32:52 -0500

    add scroll to top

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  useLocation,
 } from 'react-router-dom';
 import { accessToken, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
@@ -31,6 +32,19 @@ function App() {
 
     catchErrors(fetchData());
   }, []);
+
+  // Scroll to top of page when changing routes
+  // https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -40,6 +54,8 @@ function App() {
           </a>
         ) : (
           <BrowserRouter>
+            <ScrollToTop />
+
             <Routes>
               <Route 
                 path="/top-artists"
